df94ef2f3822f8bfc7d646cb7830c908b275fcb0
Anish Sekhar <pogchampoo24@gmail.com>
2021-11-11 17:09:56 +0530

    added js functionality to check if game is over

--- index.js
+++ index.js
@@ -13,7 +13,122 @@ function changeCurrPlayer() {
   }
 }
 
+function changeGameStatusMessage() {
+  console.log("game over");
+}
+
+function checkRows() {
+  if (
+    gameState[0] != null &&
+    gameState[1] != null &&
+    gameState[2] != null &&
+    gameState[0] === gameState[1] &&
+    gameState[1] === gameState[2]
+  ) {
+    winner = gameState[0];
+    return true;
+  }
+
+  if (
+    gameState[3] != null &&
+    gameState[4] != null &&
+    gameState[5] != null &&
+    gameState[3] === gameState[4] &&
+    gameState[4] === gameState[5]
+  ) {
+    winner = gameState[3];
+    return true;
+  }
+
+  if (
+    gameState[6] != null &&
+    gameState[7] != null &&
+    gameState[8] != null &&
+    gameState[6] === gameState[7] &&
+    gameState[7] === gameState[8]
+  ) {
+    winner = gameState[6];
+    return true;
+  }
+  return false;
+}
+
+function checkColumns() {
+  if (
+    gameState[0] != null &&
+    gameState[3] != null &&
+    gameState[6] != null &&
+    gameState[0] === gameState[3] &&
+    gameState[3] === gameState[6]
+  ) {
+    winner = gameState[0];
+    return true;
+  }
+
+  if (
+    gameState[1] != null &&
+    gameState[4] != null &&
+    gameState[7] != null &&
+    gameState[1] === gameState[4] &&
+    gameState[4] === gameState[7]
+  ) {
+    winner = gameState[1];
+    return true;
+  }
+
+  if (
+    gameState[2] != null &&
+    gameState[5] != null &&
+    gameState[8] != null &&
+    gameState[2] === gameState[5] &&
+    gameState[5] === gameState[8]
+  ) {
+    winner = gameState[2];
+    return true;
+  }
+  return false;
+}
+
+function checkDiagonals() {
+  if (
+    gameState[0] != null &&
+    gameState[4] != null &&
+    gameState[8] != null &&
+    gameState[0] === gameState[4] &&
+    gameState[4] === gameState[8]
+  ) {
+    winner = gameState[0];
+    return true;
+  }
+
+  if (
+    gameState[2] != null &&
+    gameState[4] != null &&
+    gameState[6] != null &&
+    gameState[2] === gameState[4] &&
+    gameState[4] === gameState[6]
+  ) {
+    winner = gameState[2];
+    return true;
+  }
+  return false;
+}
+
+function checkGameOver() {
+  if (checkRows() || checkColumns() || checkDiagonals()) {
+    isGameOver = true;
+    for (let i = 1; i < 10; i++) {
+      document.getElementsByClassName(
+        "ticTacToeSquare" + i.toString()
+      )[0].style.cursor = "auto";
+    }
+    changeGameStatusMessage();
+  }
+}
+
 function tileClicked(tileNumber) {
+  if (isGameOver || gameState[tileNumber - 1] !== null) return;
+
   let className = "hidden" + currPlayer + tileNumber.toString();
   let hiddenElement = document.getElementsByClassName(className)[0];
   hiddenElement.style.visibility = "visible";
@@ -29,7 +144,10 @@ function tileClicked(tileNumber) {
 
   gameState[tileNumber - 1] = currPlayer;
   changeCurrPlayer();
+  checkGameOver();
 }
 
 currPlayer = "X";
 gameState = Array(9).fill(null);
+isGameOver = false;
+winner = null;
